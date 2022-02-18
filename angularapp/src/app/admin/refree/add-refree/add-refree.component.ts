e9d4e99b0e1c14ec3d78c750a258798d26615782
mallikagopu <saimallikasai12345@gmail.com>
2022-02-18 17:22:55 +0000

    Adim-referee complete -mallika

--- angularapp/src/app/admin/refree/add-refree/add-refree.component.ts
+++ angularapp/src/app/admin/refree/add-refree/add-refree.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {RefereeModel} from '../referee.model';
+import { RefreeService } from '../refree.service';
 @Component({
   selector: 'app-add-refree',
   templateUrl: './add-refree.component.html',
-  styleUrls: ['./add-refree.component.css']
+  styles: [
+  ]
 })
 export class AddRefreeComponent implements OnInit {
-
-  constructor() { }
+  AddForm!:FormGroup
+  constructor(private service:RefreeService,private route:Router) { }
 
   ngOnInit(): void {
+    this.AddForm=new FormGroup(
+      {
+        _refreeId:new FormControl(null,Validators.required),
+        _refreeName: new FormControl(null,Validators.required),
+        _noOfMatches: new FormControl(null,Validators.required)
+      }
+    )
+  }
+  addRefree(){
+    const body: RefereeModel={
+      refreeId:this.AddForm?.get('_refreeId')?.value,
+      refereeName:this.AddForm?.get('_refreeName')?.value,
+      noOfMatches:this.AddForm?.get('_noOfMatches')?.value
+    }
+    this.service.addRefree(body).subscribe(x=>{
+      console.log(body);
+      console.log(x);
+      setTimeout(()=>{
+        this.route.navigate(["admin/refree"]);
+      },100)
+    });
+
   }
 
 }
