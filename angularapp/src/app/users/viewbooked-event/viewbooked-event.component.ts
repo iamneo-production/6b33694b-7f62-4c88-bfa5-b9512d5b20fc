f0bde94e804168f39c497a31174194116e2521bf
ManasaKesani <kesanimanasa2000@gmail.com>
2022-02-20 12:38:12 +0000

    viewbooked-event component-Manasa Kesani

--- angularapp/src/app/users/viewbooked-event/viewbooked-event.component.ts
+++ angularapp/src/app/users/viewbooked-event/viewbooked-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-viewbooked-event',
@@ -11,5 +11,44 @@ export class ViewbookedEventComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+}*/
+import { Component, OnInit } from '@angular/core';
+import { BookeventService } from '../bookevent.service';
+
+@Component({
+  selector: 'app-viewbooked-event',
+  templateUrl: './viewbooked-event.component.html',
+  styles: [
+  ]
+})
+export class ViewbookedEventComponent implements OnInit {
+  bookList:any=[];
+  temp!:any;
+  constructor(private service:BookeventService) { 
+    this.getdetails();
+  }
+
+  ngOnInit(): void {
+  }
+  getdetails(){
+    this.service.getbooking().subscribe(data=>{
+      this.bookList=data;
+    })
+  }
+  delete(index:number)
+  {
+    let temp=this.bookList[index].bookingId
+
+this.service.deleteBooking(temp).subscribe(x=>{
+  console.log(x);
+})
+this.service.getbooking();
+  }
+  edit(index:number)
+  {
+    this.service.editBody=this.bookList[index];
+  }
+
 
 }
