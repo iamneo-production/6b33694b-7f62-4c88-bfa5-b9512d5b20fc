db876668ec731702483b5fc12b719fcf34fe0f29
Lavanyateela <lavanyateela@gmail.com>
2022-02-20 14:23:49 +0000

    Login Component completed - Lavannya

--- angularapp/src/app/auth/login/login.component.ts
+++ angularapp/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {  Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  LoginF!: FormGroup
+  auth!: boolean;
+  user!: string;
+  constructor(private routes: Router, private authservice: AuthenticationService) { }
+//This gets the credentials form the users
   ngOnInit(): void {
+    this.LoginF = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "password": new FormControl(null, [Validators.required, Validators.pattern("[a-zA-Z0-9_@#$!?></|+*]{8,12}")])
+    })
+  }
+  //Navigate to Signup page
+  toSignUp() {
+    this.routes.navigate(['signup']);
+  }
+  tohome() {
+    // this.routes.navigate(['homepage']);
+    let _email = this.LoginF.get('email')?.value;
+    let _password = this.LoginF.get('password')?.value;
+
+    //Admin Check
+    if (_email === 'admin@gmail.com') {
+
+      this.authservice.AdminCheck(_email, _password).subscribe(data => {
+        this.auth = data.allowed;
+              });
+      //Navigation after authentication
+      setTimeout(() => {
+        if (this.auth) { 
+          this.routes.navigate(['../admin/view'])
+        }
+        else {
+          alert("Access Denied")
+        }
+      }, 100);
+    }
+    //Authentication for Customer using the loginCheck 
+    else {
+      this.authservice.LoginCheck(_email, _password).subscribe(data => {
+        this.auth = data.allowed;
+        this.user = data.user;
+
+      });
+      
+      //Navigate after validation
+      setTimeout(() => {
+        if (this.auth) {
+          this.routes.navigate(["user", "homepage"]);
+        } else {
+          alert("User Not Found");
+        }
+      }, 100);
+
+    }
+
   }
 
 }
