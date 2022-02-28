c7d415e4a613accfe5e665e0e9ebe8f9225f998b
rehmanmohammed <61676921+rehmanmohammed@users.noreply.github.com>
2022-02-28 23:56:20 +0530

    Update admin.component.ts

--- angularapp/src/app/admin/admin.component.ts
+++ angularapp/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../auth/authentication.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authser: AuthenticationService) { }
 
   ngOnInit(): void {
   }
-
+  logout(){
+    this.authser.route.navigate(['login']);
+  }
 }
