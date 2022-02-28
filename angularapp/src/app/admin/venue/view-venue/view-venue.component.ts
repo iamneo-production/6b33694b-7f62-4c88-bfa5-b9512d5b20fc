785447981a52bc4b0168d00bafce4c16ded57374
rehmanmohammed <61676921+rehmanmohammed@users.noreply.github.com>
2022-03-01 00:01:08 +0530

    Update view-venue.component.ts

--- angularapp/src/app/admin/venue/view-venue/view-venue.component.ts
+++ angularapp/src/app/admin/venue/view-venue/view-venue.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VenueModel } from 'src/app/shared/venue.model';
+import { VenueserviceService } from '../venueservice.service';
 
 @Component({
   selector: 'app-view-venue',
   templateUrl: './view-venue.component.html',
-  styleUrls: ['./view-venue.component.css']
+  styles: [
+  ]
 })
 export class ViewVenueComponent implements OnInit {
 
-  constructor() { }
+  venues!: any[];
+  Venues!: VenueModel[];
+  venueIdd!: number
+  constructor(private service: VenueserviceService, private route: Router, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getDetails();
+
+  }
+  // get all the details from the service 
+  getDetails() {
+    this.service.getVenue().subscribe(data => {
+      this.venues = data;
+      this.Venues = data;
+
+    })
+  }
+  ToUpdate(index: number) {
+    this.service.editVenue = this.Venues[index];
+
+    this.route.navigate(['admin/edit'])
+  }
+  delete(index: number) {
+    this.service.deleteVenueId = this.Venues[index];
+    this.venueIdd = index;
+  }
+  deletethis() {
+    let params = this.venueIdd;
+    console.log(params);
+    this.service.deleteVenue(params).subscribe(x => {
+
+      console.log(x);
+
+    });
+    setTimeout(() => {
+      this.route.navigate(["admin/view"]);
+    }, 100);
+
+
+
   }
 
 }
