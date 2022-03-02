4a726b0f9c291ab5ff1d02169f5cd0bbd963b354
Ranjithrocky <2k18cse082@kiot.ac.in>
2022-03-02 16:50:24 +0000

    Login model - Ranjith

---
+++ dotnetapp/WebApp/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Baseball.Model
+{
+    public partial class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
