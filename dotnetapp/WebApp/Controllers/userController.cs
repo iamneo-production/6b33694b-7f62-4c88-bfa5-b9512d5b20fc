ac5268caf20c828061492402112a7a25817c67fa
Dakshan444 <97862909+Dakshan444@users.noreply.github.com>
2022-03-09 21:06:20 +0530

    userController Updated -Dhakshan

--- dotnetapp/WebApp/Controllers/userController.cs
+++ dotnetapp/WebApp/Controllers/userController.cs
@@ -63,3 +63,92 @@ namespace Baseball.Controllers
                 });
             }
         }
+         [HttpPost("bookEvent")]
+        public IActionResult BookEvent([FromBody] EventModel eventobj)
+        {
+            if(eventobj == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.EventModels.Add(eventobj);
+                _context.SaveChanges();
+                return Ok(
+                    new
+                    {
+                        StatusCode=200,
+                        Message ="Event added"
+
+                    });
+            }
+        }
+        [HttpPut("editEvent/{id}")]
+        public IActionResult EditEvent([FromBody] EventModel eventobj)
+        {
+            if(eventobj==null)
+            {
+                return BadRequest();
+            }
+            var user =_context.EventModels.AsNoTracking().FirstOrDefault(x=>x.EventId == eventobj.EventId);
+         if(user==null)
+            {
+                return NotFound(
+                    new
+                    {
+                        StatusCode=400,
+                        Message="User Not Found"
+                    });
+            }
+            else
+            {
+                _context.Entry(eventobj).State=EntityState.Modified;
+                _context.SaveChanges();
+                return Ok(
+                    new
+                    {
+                        StatusCode=200,
+                        Message="Event Updated"
+                    });
+
+            }
+        }
+        [HttpDelete("deleteEvent/{id}")]
+        public IActionResult CancelEvent(int id )
+        {
+            var user = _context.EventModels.Find(id);
+            if (user == null)
+            {
+                return NotFound(new
+                {
+                    StausCode = 404,
+                    Message = "User Not Found"
+                });
+            }
+            else
+            {
+                _context.Remove(user);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "Deleted successfully"
+                });
+            }
+
+        }
+        [HttpGet("getSchedule")]
+        public IActionResult ViewBookedEvent()
+        {
+            var events = _context.EventModels.AsQueryable();
+            return Ok(new
+            {
+                StatusCode = 200,
+                Employee_Details = events
+            });
+        }
+
+    }
+}
+
+        
