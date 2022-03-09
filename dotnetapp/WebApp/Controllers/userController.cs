60608f6675f16eb2cacf751f6a516a4df7accaaf
rehmanmohammed <61676921+rehmanmohammed@users.noreply.github.com>
2022-03-09 21:02:23 +0530

    userController -Rehman

---
+++ dotnetapp/WebApp/Controllers/userController.cs
@@ -0,0 +1,65 @@
+using Baseball.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Baseball.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class userController : ControllerBase
+    {
+        private readonly baseball_tContext _context;
+        public userController(baseball_tContext baseball_TContext)
+        {
+            _context = baseball_TContext;
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel userobj)
+        {
+
+            if(userobj == null)
+            {
+                return BadRequest();
+                
+            }
+            else
+            {
+                var user = _context.LoginModels.Where(a => a.Email == userobj.Email && a.Password == userobj.Password).FirstOrDefault();
+                if(user == null)
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "True"
+                    });
+                }
+                else
+                {
+                    return NotFound(new
+                    {
+                        StatusCode = 404,
+                        Message = "False"
+                    });
+                }
+            }
+        }
+        [HttpPost("signup")]
+        public IActionResult SignUp([FromBody] UserModel userobj)
+        {
+            if (userobj == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.UserModels.Add(userobj);
+                _context.SaveChanges();
+                return Ok( new
+                {
+                    StatusCode=200,
+                    Message="User Added"
+                });
+            }
+        }
