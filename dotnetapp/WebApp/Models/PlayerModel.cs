638eaf58e2ed66b56134b7cf74cc6d342185ce88
Ranjithrocky <68370826+Ranjithrocky@users.noreply.github.com>
2022-03-27 18:00:04 +0530

    PalyerModel added

---
+++ dotnetapp/WebApp/Models/PlayerModel.cs
@@ -0,0 +1,16 @@
+namespace Baseball.Model
+{
+    public class PlayerModel
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public int Age { get; set; }
+
+            public string Gender { get; set; }
+
+            public int TeamId { get; set; }    
+
+    }
+}
